8a2acc40aa312ce3599f998e6c9861497f142674
Supawee Bom Ippoodom <i.supawee@gmail.com>
2025-06-04 02:55:59 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,10 @@ set -e
 echo "✅ start.sh is running..."
 
 # Fix file permissions (good for both Laravel & CI2)
-chown -R www-data:www-data /var/www/html
-chmod -R 755 /var/www/html
+#chown -R www-data:www-data /var/www/html
+#chmod -R 755 /var/www/html
+find /var/www/html -type d -exec chmod 755 {} \;
+find /var/www/html -type f -exec chmod 644 {} \;
 
 # Composer install (if Laravel is present)
 if [ -f /var/www/html/artisan ]; then
